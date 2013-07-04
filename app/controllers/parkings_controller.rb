ce1f42bbb9461fd04ed0fa943b4a4777c994afc3
Kevin Sołtysiak <kevin@ksol.fr>
2013-07-04 21:15:57 +0200

    Do not load stuff in view; delete useless file

--- app/controllers/parkings_controller.rb
+++ app/controllers/parkings_controller.rb
@@ -1,9 +1,16 @@
 class ParkingsController < ApplicationController
-  
+  before_filter :load_data
+
   def index
   end
 
   def embed
     render :action => "index"
+  end
+
+  private
+
+  def load_data
+    @parkings = Parking.all
   end
 end
