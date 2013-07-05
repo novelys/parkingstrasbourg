21aa62241a73d2df9c3cff5d71719b91a64d297b
Kevin Sołtysiak <kevin@ksol.fr>
2013-07-05 13:17:40 +0200

    Clean the name

--- app/serializers/parking_serializer.rb
+++ app/serializers/parking_serializer.rb
@@ -1,6 +1,7 @@
 class ParkingSerializer < ActiveModel::Serializer
-  attributes :name, :available, :total, :opened, :trend, :lat, :lng
+  attributes :available, :total, :opened, :trend, :lat, :lng
   attribute :external_id, key: :id
+  attribute :internal_name, key: :name
 
   def opened
     !object.is_closed?
