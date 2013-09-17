dfa1ffaa2fdb81bb9dff5d3a053adefe4856eaf8
Kevin Sołtysiak <kevin@ksol.fr>
2013-09-17 12:05:00 +0200

    Deployment config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,69 +1,13 @@
+require 'capistrano/novelys'
+load 'novelys/rbenv'
+load 'novelys/unicorn'
+load 'novelys/logs'
+load 'novelys/stages'
+load 'novelys/remote_commands'
+load 'novelys/production_chain'
+load 'novelys/rails3'
+load 'novelys/mongoid'
+load 'novelys/whenever'
 load 'deploy/assets'
 
-set :application, "parkingstrasbourg"
-set :repository, "git@github.com:novelys/#{application}"
-
-set :keep_releases, 5
-
-set :scm, "git"
-set :repository_cache, "git_cache"
-set :copy_exclude, [".svn", ".DS_Store", ".git"]
-set :bundle_without,  [:development, :test, :cucumber]
-ssh_options[:forward_agent] = true
-
-set :deploy_to, "/home/parkingstrasbourg/www/#{application}"
-
-require 'capistrano/ext/multistage'
-require 'bundler/capistrano'
-
-set :stages, %w(staging production)
-
-after "deploy:update_code", "db:symlink"
-# before "deploy:assets:precompile", "db:symlink"
-
-after "deploy:setup", "db:mkdir"
-after "deploy", "deploy:cleanup"
-
-set :whenever_roles, "whenever"
-
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-
-namespace :db do
-  desc "Make symlink for database yaml"
-  task :symlink do
-    run "ln -nfs #{shared_path}/config/mongoid.yml #{current_release}/config/mongoid.yml"
-  end
-
-  desc "Create necessary directories"
-  task :mkdir do
-    run "mkdir -p #{shared_path}/config"
-  end
-end
-
-set :unicorn_binary, "bundle exec unicorn_rails"
-set :unicorn_config, "#{current_path}/config/unicorn.rb"
-set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
-
-namespace :deploy do
-  task :start, :roles => :app, :except => { :no_release => true } do
-    run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
-  end
-  task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill `cat #{unicorn_pid}`"
-  end
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
-  end
-  task :reload, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
-  end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    stop
-    start
-  end
-end
-
-        require 'bundler/setup'
-        require 'production_chain/capistrano'
-
+set(:application) { "parkingstrasbourg" }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,15 +1,5 @@
-set :default_environment, {
-  'PATH' => "/home/rails19/.rbenv/shims:/home/rails19/.rbenv/bin:$PATH"
-}
+set(:deploy_to) { '/home/rails19/www/parkingstrasbourg' }
+set(:user) { "rails19" }
+set(:runner) { "rails19" }
 
-set :user, "rails19"
-set :runner, "rails19"
-set :use_sudo, false
-set :rails_env, "staging"
-
-set :branch, "staging"
-
-role :app, "staging.novelys.com", :primary => true
-role :web, "staging.novelys.com", :primary => true
-role :indexer, "staging.novelys.com"
-role :db, "staging.novelys.com", :primary => true
+server 'staging.novelys.com', :app, :web, :db, primary: true
