9381f2e82994b11f4f28222bfecc9b2498615edd
Kevin Sołtysiak <kevin@ksol.fr>
2013-10-23 22:34:31 +0200

    Should fix iframe issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,10 @@ Parkingstrasbourg::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL'
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
