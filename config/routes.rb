7c632dffd0de1df57d0346e5be494bb2bd272d1b
Yann Klis <yann.klis@novelys.com>
2014-01-02 15:03:10 +0100

    continue beautification

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Parkingstrasbourg::Application.routes.draw do
   root :to => 'parkings#index'
 
   get 'embed' => 'parkings#embed'
-  get 'faq'  => 'parkings#faq'
+  get 'faq'   => 'parkings#faq'
   get 'ideal' => 'parkings#ideal'
 
   resources :parkings, only: [:index, :show]
