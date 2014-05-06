5587998965b5c72514d334030a3559bb9df8c37f
Kevin Sołtysiak <kevin@ksol.fr>
2014-05-06 11:27:10 +0200

    Forecast delay

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -69,7 +69,7 @@ class Parking
   # Returns an array of forecasted availabilities
   def forecast(delay)
     time = delay.minutes.since
-    steps = (22..30).to_a.map { |num| time - num.weeks}
+    steps = (0..30).to_a.map { |num| time - num.weeks}
     ary = steps.map { |time| self.availabilities.around(time) }.flatten
     ary.reject! &:is_closed?
     ary.sort_by! &:available
