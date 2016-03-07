7ed3e399ada9226a026fa87784511d12edb5387c
Yann Klis <yann.klis@novelys.com>
2016-03-07 09:13:26 +0100

    deleted X-Frame-Options setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :set_locale
+  before_action :set_locale
+  before_action :allow_iframe
 
-  private
+private
+
+  def allow_iframe
+    response.headers.delete "X-Frame-Options"
+  end
 
   def set_locale
     I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
